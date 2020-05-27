1ec7ee35e7ca275af1225b5807db62fbaf7fdae8
Jesse Gan <jesse13579@gmail.com>
2020-05-27 14:32:07 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,16 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end 
+
+def batch_badge_creator(attendees)
+  attendees.map do |attendee|
+    badge_maker(attendee)
+  end
+end 
+
+def assign_rooms(attendees)
+
+  attendees.each_with_index.map do |attendee,i|
+    "Hello, #{attendee}! You'll be assigned to room #{i+1}!"
+  end 
+end 
